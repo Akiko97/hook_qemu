d0738004f052a4e29ed3f78c0fb66edf375a591a
Shinonome Akiko <shinonomeakiko@gmail.com>
2019-05-07 14:53:52 +0800

    update

--- modtools.sh
+++ modtools.sh
@@ -1,18 +1,22 @@
-#!/bin/sh
-if [ -n "$1" ]
+#!/bin/bash
+if [[ $USER == "root" ]]
 then
-	case "$1" in
-		load)
-			cpstings_address=`grep copy_strings.isra /proc/kallsyms | awk {'print $1'}`
-			/sbin/insmod ./hook_qemu_mod.ko hook_address=0x$cpstings_address
-			echo "dmesg: `dmesg | tail -n1`" ;;
-		remove)
-			/sbin/rmmod hook_qemu_mod
-			echo "dmesg: `dmesg | tail -n1`" ;;
-		*)
-			echo "Usage: $0 [load | remove]" ;;
-	esac
+	if [ -n "$1" ]
+	then
+		case "$1" in
+			load)
+				cpstings_address=`grep copy_strings.isra /proc/kallsyms | awk {'print $1'}`
+				/sbin/insmod ./hook_qemu_mod.ko hook_address=0x$cpstings_address
+				echo "dmesg: `dmesg | tail -n1`" ;;
+			remove)
+				/sbin/rmmod hook_qemu_mod
+				echo "dmesg: `dmesg | tail -n1`" ;;
+			*)
+				echo "Usage: sudo $0 [load | remove]" ;;
+		esac
+	else
+		echo "Usage: sudo $0 [load | remove]"
+	fi
 else
-	echo "Usage: $0 [load | remove]"
+	echo "this script must run as root user"
 fi
-
